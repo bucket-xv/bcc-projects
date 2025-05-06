06b96a273dae5fd2a62cbbf8ff9f15b5f4b3c9b5
Chenhao Xu <xv_chen_hao@163.com>
2025-05-06 10:28:16 +0800

    use veth

--- scripts/veth.sh
+++ scripts/veth.sh
@@ -10,16 +10,16 @@ sudo ip link set veth-red netns red
 sudo ip link set veth-blue netns blue
 
 # Set up veth0
-sudo ip netns exec red ip addr add 192.168.15.1/24 dev veth-red
-sudo ip netns exec blue ip addr add 192.168.15.2/24 dev veth-blue
+sudo ip netns exec red ip addr add 10.0.1.1/24 dev veth-red
+sudo ip netns exec blue ip addr add 10.0.1.2/24 dev veth-blue
 
 sudo ip netns exec red ip link set veth-red up
 sudo ip netns exec blue ip link set veth-blue up
 
 # Add routes
-sudo ip netns exec red ip route add 192.168.15.0/24 dev veth-red
-sudo ip netns exec blue ip route add 192.168.15.0/24 dev veth-blue
+sudo ip netns exec red ip route add 10.0.1.0/24 dev veth-red
+sudo ip netns exec blue ip route add 10.0.1.0/24 dev veth-blue
 
 # Ping
-sudo ip netns exec red ping 192.168.15.2
-sudo ip netns exec blue ping 192.168.15.1
+sudo ip netns exec red ping 10.0.1.2
+sudo ip netns exec blue ping 10.0.1.1
